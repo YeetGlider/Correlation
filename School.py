1dd2fe97232f337c43a92d7d28a5bfa58678509c
YeetGlider <72340148+YeetGlider@users.noreply.github.com>
2022-10-03 14:48:53 +0530

    Add files via upload

---
+++ School.py
@@ -0,0 +1,9 @@
+import plotly.express as px
+import csv
+
+with open("Student Marks vs Days Present.csv") as csv_file:
+    df = csv.DictReader(csv_file)
+    fig = px.scatter(df,x="Roll No", y="Marks In Percentage", color="Days Present")
+    fig.show()
+
+
